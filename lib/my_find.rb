097a5240ab0888233823468280139e08c844b4ff
dwane richards <dwanerich@gmail.com>
2019-12-17 03:48:20 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
 def my_find(collection)
-  new_array = []
     collection.each do |element|
-    new_array << element if yield(element) == true && new_array.length == 0
+    return element if yield(element) == true && new_array.length == 0
     end
     new_array.first
 end
