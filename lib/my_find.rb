c22bc5f4e426487165c36c1f78f46586db2d91af
dwane richards <dwanerich@gmail.com>
2019-12-17 03:45:17 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   new_array = []
     collection.each do |element|
-    new_array << element if yield(element) == true && new_array.length < 1
+    new_array << element if yield(element) == true && new_array.length == 0
     end
     new_array
 end
